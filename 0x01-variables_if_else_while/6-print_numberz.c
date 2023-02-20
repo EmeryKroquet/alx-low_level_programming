ccdb75b44a9b65c9e640872c13d5462534bdd6e8
EmeryKroquet <emery.kroquet@gmail.com>
2023-02-20 11:11:48 -0800

    6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
  * Return: 0
  */
 
-int main() {
+int main(void){
     int i;
-    for (i = 0; i < 10; i++) {
-        putchar(i + '0');
-    }
+    for(i = 0; i < 10; i++)
+	    putchar(i + '0');
+
     putchar('\n');
     return 0;
 }
