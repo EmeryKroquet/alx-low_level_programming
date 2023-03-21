afe4dd87c9a3617a35531892d28cb529f6b1c5e8
EmeryKroquet <emery.kroquet@gmail.com>
2023-03-21 04:37:28 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
-#include "_putchar.h"
+#include "main.h"
 /**
- * main - Write a program that prints Holberton, followed by a new*
+ * main - Entry point
  *
- *
- * Return: On success 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char *sh = "main";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
 	_putchar('\n');
 
 	return (0);
