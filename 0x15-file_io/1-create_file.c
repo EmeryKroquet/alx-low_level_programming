7231fee9cc0ef191e3f8308cd6e85ebd32783490
EmeryKroquet <emery.kroquet@gmail.com>
2023-04-09 06:15:11 -0700

    cretae file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,33 @@
 #include "main.h"
 
 /**
- * create_file - create a file.
- * @filename: name of the file to create.
- * @text_content: text to write inside the file.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 if the file was created, -1 otherwise.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd_open, fd_write;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd_open = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-	if (fd_open == -1)
-		return (-1);
+
 	if (text_content != NULL)
 	{
-		fd_write = write(fd_open, text_content, _strlen(text_content));
-		if (fd_write == -1)
-			return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	close(fd_open);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
 	return (1);
-}
-
-/**
- * _strlen - calculate the lenght of a string.
- * @str: array of characters.
- *
- * Return: lenght of the string.
- */
-int _strlen(char *str)
-{
-	int count = 0;
-
-	while (*str++)
-		count++;
-	return (count);
 }
